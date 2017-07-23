d8c8f8654cb79a6c4d50413c944d476e0ab08a69
DavidHaas <cavoom@me.com>
2017-07-23 14:07:17 -0400

    greating the new users

--- server/server.js
+++ server/server.js
@@ -15,15 +15,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log('new user connected');
 
-    // listens for the createMessage event
+    // Send to the new user only
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome here',
+        createdAt: new Date().getTime()
+    });
+
+    // send to everyone else
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'someone else joined yo',
+        createdAt: new Date().getTime()
+    });
+
+    // listens for the createMessage event from client and then sends it out to everyone
     socket.on('createMessage', (message) =>{
-        console.log('createMessage', message);
+         console.log('createMessage', message);
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        });
-    })
+     });
+     // sends to everyone but the sender
+    // socket.broadcast.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    // });
+     })
+
         // listens for a disconnnect from client
         socket.on('disconnect',()=>{
             console.log('Disconnected from client');
